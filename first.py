0e8f557ea65f267ef4148e1939069147628f1b6c
physicsvaib <physicsvaib@gmail.com>
2018-02-22 12:27:09 +0530

    first commit

--- first.py
+++ first.py
@@ -1,2 +1,20 @@
-a=open('a.txt','r').readlines()
-print(a)
+class calc:
+    def s(x,y):
+    a=float(x)-float(y)
+    print(a)
+    def m(x,y):
+    b=float(x)*float(y)
+    print(a)
+
+    def a(x,y):
+    c=float(x)+float(y)
+    print(a)
+
+    def d(x,y):
+    d=float(x)/float(y)
+    print(a)
+
+a=input("enter\n")
+b=input("enter again\n")
+print("what to do\n")
+print(calc.s(a,b))
