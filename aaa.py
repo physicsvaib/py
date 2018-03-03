07613123ec9115e16d268ad914d7d75c684f1205
physicsvaib <physicsvaib@gmail.com>
2018-03-03 23:49:48 +0530

    py change

--- aaa.py
+++ aaa.py
@@ -1,1 +1,18 @@
-
+import numpy as np
+#this is the starting
+m=[]
+n=[]
+k=[]
+j=[]
+x=[m,n]
+y=[k,j]
+n=int(input("the value of N"))
+for i in range(n):
+    f=int(input("enter number"))
+    x.append(f)
+for z in range(n):
+    e=int(input("enter number"))
+    y.append(e)
+a=np.array([x])
+b=np.array([y])
+print(a,b)
--- as.py
+++ as.py
@@ -1,14 +1,8 @@
-from matplotlib import pyplot as plt
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-A = -0.75, -0.25, 0, 0.25, 0.5, 0.75, 1.0
-B = 0.73, 0.97, 1.0, 0.97, 0.88, 0.73, 0.54
-
-plt.plot(A,B)
-for xy in zip(A, B):
-    ax.annotate('(%s, %s)' % xy, xy=xy, textcoords='data') 3
-
-
-plt.show()
+meal_cost = 12
+tip_percent = 20
+tax_percent = 8
+tip=meal_cost*tip_percent/100
+tax=meal_cost*tax_percent/100
+x=meal_cost+tip+tax
+y=5
+print("The total meal cost is {a} {b} dollars.".format(a=round(x),b=(y)))
