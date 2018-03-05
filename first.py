ca1598e3ec0a44895fbf321b02ff179a283294d1
physicsvaib <physicsvaib@gmail.com>
2018-03-06 01:03:23 +0530

    py 6/3/18

--- first.py
+++ first.py
@@ -1,5 +1,6 @@
 #importing tkinter
 from tkinter import *
+from PIL import Image,ImageTk
 #main functioning starts
 class window(Frame):
     def __init__(self,master=NONE):
@@ -17,11 +18,22 @@ class window(Frame):
         file.add_command(label="menu",command=self.x)
         menu.add_cascade(label="file",menu=file)
         edit=Menu(menu)
-        edit.add_command(label="edit",command=self.x)
+        edit.add_command(label="Show Image",command=self.show)
+        edit.add_command(label="Show Text",command=self.showt)
+
         edit.add_command(label="new")
         menu.add_cascade(label="edit",menu=edit)
     def x(self):
         exit()
+    def show(self):
+        load=Image.open('a.jpg')
+        render=ImageTk.PhotoImage(load)
+        img=Label(self,image=render)
+        img.image=render
+        img.place(x=0,y=0)
+    def showt(self):
+        text=Label(self,text="hello there")
+        text.pack()
 ro=Tk()
 app1=window(ro)
 ro.geometry('300x400')
