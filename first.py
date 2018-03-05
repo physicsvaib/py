9d97cfd142319d1c4e4c22242001a65912e8bba9
physicsvaib <physicsvaib@gmail.com>
2018-03-05 23:54:09 +0530

    py 6/3/18

--- first.py
+++ first.py
@@ -1,20 +1,31 @@
-class calc:
-    def s(x,y):
-    a=float(x)-float(y)
-    print(a)
-    def m(x,y):
-    b=float(x)*float(y)
-    print(a)
+# tkinter tutorials
+from tkinter import *
 
-    def a(x,y):
-    c=float(x)+float(y)
-    print(a)
 
-    def d(x,y):
-    d=float(x)/float(y)
-    print(a)
+class window(Frame):
+    def __init__(x, master=None):
+        Frame.__init__(x, master)
+        x.y()
 
-a=input("enter\n")
-b=input("enter again\n")
-print("what to do\n")
-print(calc.s(a,b))
+    def y(x):
+        x.master.title("new program")
+        x.pack(fill=BOTH, expand=6)
+        quit = Button(x, text="quit", command=x.exit)
+        quit.place(x=0, y=0)
+        m=Menu(x.master)
+        x.master.config(m=m)
+        file=Menu(m)
+        file.add_command(label='exit',command=x.exit)
+        m.add_cascade(label='file',m=file)
+        edit=Menu(m)
+        edit.add_command(label="save",command=x.exit)
+        edit.add_command(label='undo')
+        m.add_cascade(label='edit',m=edit)
+    def exit(self):
+        exit()
+
+
+root = Tk()
+root.geometry("500x300")
+app = window(root)
+root.mainloop()
